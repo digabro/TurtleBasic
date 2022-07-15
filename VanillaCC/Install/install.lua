b7feb33e8ac8b88838141530024c74f2ad2abf13
digabro <diego.olin@gmail.com>
2022-07-15 11:32:57 -0600

    added vanilla folder
    added vanilla folder to be able to add peripherals

--- VanillaCC/Install/install.lua
+++ VanillaCC/Install/install.lua
@@ -16,4 +16,12 @@ if (prog == 'phone' or prog == 'Phone') then
     download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/Install/reinstall.lua','reinstall')
     print('reinstaller installed')
 elseif (prog == 'turtle' or prog == 'Turtle') then
-    
+    download('https://github.com/digabro/TurtleBasic/blob/main/Turtle/turtleControl.lua')
+    print('turtleControl installed')
+    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/Install/reinstall.lua','reinstall')
+    print('reinstaller installed')
+    download('https://github.com/digabro/TurtleBasic/blob/main/Turtle/Quarry.lua', 'quarry')
+    print('quarry installed')
+    download('https://github.com/digabro/TurtleBasic/blob/main/Turtle/Summon.lua', 'summon')
+    print('summon installed')
+
