4a5acfa711a8cc4db3f0e35f01618f0f497c6ae2
digabro <diego.olin@gmail.com>
2022-07-14 18:13:23 -0600

    Update reinstall(doesnt work lmao).lua

--- reinstall(doesnt work lmao).lua
+++ reinstall(doesnt work lmao).lua
@@ -13,10 +13,10 @@ end
 
 if (prog == 'turtle' or prog == 'Turtle') then
     fs.delete('turtleControl.lua')
-    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/Turtle/turtleControl.lua?token=GHSAT0AAAAAABWTABYZDD4NRMSMHQEDNLYEYWQV45Q','turtleControl')
+    download('https://github.com/digabro/TurtleBasic/blob/main/Turtle/turtleControl.lua','turtleControl')
     print('turtleControl.lua reinstalled')
 elseif (prog == 'phone' or prog == 'Phone') then
     fs.delete('phoneControl.lua')
-    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/Pocket/phoneControl.lua?token=GHSAT0AAAAAABWTABYY6OANYTK556OLU4BEYWQV5CA','phoneControl')
+    download('https://github.com/digabro/TurtleBasic/blob/main/Pocket/phoneControl.lua','phoneControl')
     print('phoneControl.lua reinstalled')
 end
