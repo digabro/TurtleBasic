ae01460af09e7c8d28acfd835d7678896467b684
digabro <diego.olin@gmail.com>
2022-07-15 11:44:48 -0600

    replace install links

--- VanillaCC/Install/install.lua
+++ VanillaCC/Install/install.lua
@@ -11,17 +11,17 @@ function download(url, file)
 end
 
 if (prog == 'phone' or prog == 'Phone') then
-    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/VanillaCC/Pocket/phoneControl.lua','phoneControl')
+    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/VanillaCC/Phone/phoneControl.lua','phoneControl')
     print('phoneControl.lua installed')
     download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/VanillaCC/Install/reinstall.lua','reinstall')
     print('reinstaller installed')
 elseif (prog == 'turtle' or prog == 'Turtle') then
-    download('https://github.com/digabro/TurtleBasic/blob/main/VanillaCC/Turtle/turtleControl.lua')
+    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/VanillaCC/Turtle/turtleControl.lua', 'turtleControl')
     print('turtleControl installed')
     download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/VanillaCC/Install/reinstall.lua','reinstall')
     print('reinstaller installed')
-    download('https://github.com/digabro/TurtleBasic/blob/main/VanillaCC/Turtle/Quarry.lua', 'quarry')
+    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/VanillaCC/Turtle/Quarry.lua', 'quarry')
     print('quarry installed')
-    download('https://github.com/digabro/TurtleBasic/blob/main/VanillaCC/Turtle/Summon.lua', 'summon')
+    download('https://raw.githubusercontent.com/digabro/TurtleBasic/main/VanillaCC/Turtle/Summon.lua', 'summon')
     print('summon installed')
 end
